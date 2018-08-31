5ecae581cda1d98cc6f71150ba7ccdc1d77a5a53
Fernando Aguilar <fernando@aguilar.net.br>
2018-08-31 19:45:00 -0300

    Add settings for vscode

---
+++ vscode.js
@@ -0,0 +1,10 @@
+{
+    "telemetry.enableTelemetry": false,
+    "editor.tabSize": 2,
+    "html.format.enable": true,
+    "editor.minimap.enabled": false,
+    "editor.snippetSuggestions": "top",
+    "atomKeymap.promptV3Features": true,
+    "editor.multiCursorModifier": "ctrlCmd",
+    "editor.formatOnPaste": true,
+}
